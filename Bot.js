896dd485a544917c3808d3769b2f33444a20ec12
sugarbabyboy <46473273+sugarbabyboy@users.noreply.github.com>
2019-07-31 20:31:30 +0300

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,1 +1,18 @@
 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setActivity('', { type: 'WATCHING' })
+});
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+client.login('token');
